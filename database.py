44afd5aa15b701d1599bf8c3c5c589754c9863e0
k4doXxx <150913620+k4doXxx@users.noreply.github.com>
2024-03-21 12:08:44 +0100

    database.py

--- database.py
+++ database.py
@@ -14,7 +14,73 @@ class Database:
     # Méthode pour configurer les tables de la base de données
     def setup_tables(self):
         # Création des tables si elles n'existent pas déjà
-        pass
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS employes (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nom TEXT,
+            prenom TEXT NOT NULL,
+            login TEXT UNIQUE,
+            mdp TEXT,
+            email TEXT UNIQUE,
+            numero NUMERIC UNIQUE,
+            embauche DATE,
+            mdp_creation_date DATE
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS unite (
+            id_unitee INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            region TEXT,
+            nb_employe NUMERIC,
+            date_de_creation DATE
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS metier (
+            id_metier INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            id_collaborateur INTEGER,
+            id_medecin INTEGER,
+            id_scientifique INTEGER,
+            id_commercial INTEGER,
+            FOREIGN KEY(id_collaborateur) REFERENCES collaborateur(id_collaborateur),
+            FOREIGN KEY(id_medecin) REFERENCES medecin(id_medecin),
+            FOREIGN KEY(id_scientifique) REFERENCES scientifique(id_scientifique),
+            FOREIGN KEY(id_commercial) REFERENCES commercial(id_commercial)
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS scientifique (
+            id_scientifique INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            code_projet NUMERIC UNIQUE,
+            nvAcces TEXT,
+            nb_unite NUMERIC
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS commercial (
+            id_commercial INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nvAcces TEXT
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS medecin (
+            id_medecin INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nvAcces TEXT
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS collaborateur (
+            id_collaborateur INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nvAcces TEXT
+        )
+        ''')
+        self.conn.commit()
 
     # Méthode pour insérer un nouvel employé dans la base de données
     def insert_employee(self, prenom, nom, login, mdp, email, numero, date_embauche, mdp_creation_date):
